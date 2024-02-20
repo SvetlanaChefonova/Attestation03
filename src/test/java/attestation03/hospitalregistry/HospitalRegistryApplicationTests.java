4b86757f0fed4eb0404f08d3e4e8d6a7e06f70bc
SvetlanaChefonova <svetlana_chefonova@mail.ru>
2024-02-20 12:17:49 +0300

    first commit

---
+++ src/test/java/attestation03/hospitalregistry/HospitalRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package attestation03.hospitalregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
